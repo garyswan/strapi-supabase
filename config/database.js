5c85c1b8ba9f63ea587ce6f52a8dd95a381c3d52
Gary Swanepoel <gary@pixelpixel.com.au>
2022-07-04 13:04:57 +1000

    init

---
+++ config/database.js
@@ -0,0 +1,49 @@
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+// strapi-api/config/database.js
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'postgres'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'postgres'),
+//       // schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "db.whkacgsmmzgbsyrsjmxh.supabase.co"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "sEhnybZtq3bnjm@"),
+      // schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
+    },
+    debug: false,
+  },
+});
